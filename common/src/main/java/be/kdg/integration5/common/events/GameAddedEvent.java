dd7280a4f0574808535a05ea7d100ba3af52abc5
Mykhailo Kruts <mykhailo.kruts@student.kdg.be>
2024-11-25 12:56:57 +0100

    Add imageUrl to game created event

--- common/src/main/java/be/kdg/integration5/common/events/GameAddedEvent.java
+++ common/src/main/java/be/kdg/integration5/common/events/GameAddedEvent.java
@@ -2,7 +2,7 @@ package be.kdg.integration5.common.events;
 
 import java.util.List;
 
-public record GameAddedEvent(String gameName, String frontendUrl, String backendApiUrl, List<GameRule> rules) {
+public record GameAddedEvent(String gameName, String frontendUrl, String backendApiUrl, String gameImageUrl, List<GameRule> rules) {
     public record GameRule(String rule, String description) {
 
     }
