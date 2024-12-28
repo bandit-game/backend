b3c2d4371e03c24076bad9763072706a63332a9e
Mykhailo Kruts <mykhailo.kruts@student.kdg.be>
2024-12-28 20:42:32 +0100

    Set-up Statistics Context

---
+++ StatisticsContext/build.gradle.kts
@@ -0,0 +1,38 @@
+plugins {
+    id("module-config")
+}
+
+extra["snippetsDir"] = file("build/generated-snippets")
+extra["springCloudAzureVersion"] = "5.18.0"
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-amqp")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-jdbc")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("com.azure.spring:spring-cloud-azure-starter")
+    implementation("com.azure.spring:spring-cloud-azure-starter-jdbc-postgresql")
+    implementation("com.azure.spring:spring-cloud-azure-starter-storage")
+    implementation("org.springframework.boot:spring-boot-starter-webflux")
+    implementation("org.springframework.boot:spring-boot-starter-security")
+    testImplementation("io.projectreactor:reactor-test")
+    testImplementation("org.springframework.security:spring-security-test")
+    compileOnly("org.projectlombok:lombok")
+    runtimeOnly("org.postgresql:postgresql")
+    annotationProcessor("org.projectlombok:lombok")
+    implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
+
+    testImplementation("org.springframework.boot:spring-boot-starter-test") // Includes JUnit 5
+    testImplementation("org.springframework.boot:spring-boot-testcontainers") // For integration tests with Testcontainers
+
+}
+
+dependencyManagement {
+    imports {
+        mavenBom("com.azure.spring:spring-cloud-azure-dependencies:${property("springCloudAzureVersion")}")
+    }
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
---
+++ StatisticsContext/src/main/java/be/kdg/integration5/statisticscontext/StatisticsContextApplication.java
@@ -0,0 +1,11 @@
+package be.kdg.integration5.statisticscontext;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StatisticsContextApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(StatisticsContextApplication.class, args);
+    }
+}
