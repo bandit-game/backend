549cd62aaffd29ee5f84a87b7bc7c4f7b518e5fe
Mykhailo Kruts <mykhailo.kruts@student.kdg.be>
2025-01-01 20:36:06 +0100

    Prepare for future game initialization

--- StatisticsContext/src/main/java/be/kdg/integration5/statisticscontext/domain/Game.java
+++ StatisticsContext/src/main/java/be/kdg/integration5/statisticscontext/domain/Game.java
@@ -1,14 +1,16 @@
 package be.kdg.integration5.statisticscontext.domain;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
-@AllArgsConstructor
 public class Game {
     private GameId gameId;
     private String name;
 
+    public Game(GameId gameId, String name) {
+        this.gameId = gameId;
+        this.name = name.toLowerCase().replace(" ", "_");
+    }
 }
