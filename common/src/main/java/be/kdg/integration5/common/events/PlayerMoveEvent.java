27d8d8ed8fe1717de370f3f8c2d6d9ad09f64fdd
Mykhailo Kruts <mykhailo.kruts@student.kdg.be>
2025-01-02 23:19:43 +0100

    Update event to reflect correct naming

--- common/src/main/java/be/kdg/integration5/common/events/PlayerMoveEvent.java
+++ common/src/main/java/be/kdg/integration5/common/events/PlayerMoveEvent.java
@@ -4,9 +4,9 @@ import java.time.LocalDateTime;
 import java.util.Objects;
 import java.util.UUID;
 
-public record PlayerMoveEvent(UUID gameId, UUID playerId, UUID nextPlayerId, LocalDateTime timestamp) {
+public record PlayerMoveEvent(UUID lobbyId, UUID playerId, UUID nextPlayerId, LocalDateTime timestamp) {
     public PlayerMoveEvent {
-        Objects.requireNonNull(gameId);
+        Objects.requireNonNull(lobbyId);
         Objects.requireNonNull(playerId);
         Objects.requireNonNull(nextPlayerId);
         Objects.requireNonNull(timestamp);
