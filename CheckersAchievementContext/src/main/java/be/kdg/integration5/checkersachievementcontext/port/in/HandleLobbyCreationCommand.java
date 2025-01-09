1d69e56bae03058d748fad0f19a086101f0c7df5
Oleksandr Shmelov <oleksandr.shmelov@student.kdg.be>
2025-01-09 17:58:39 +0100

    Removed old implementations of HandleLobbyCreationUseCase; Fixed data.sql

--- CheckersAchievementContext/src/main/java/be/kdg/integration5/checkersachievementcontext/port/in/HandleLobbyCreationCommand.java
+++
@@ -1,9 +0,0 @@
-package be.kdg.integration5.checkersachievementcontext.port.in;
-
-import be.kdg.integration5.checkersachievementcontext.domain.GameId;
-import be.kdg.integration5.checkersachievementcontext.domain.PlayerId;
-
-import java.util.List;
-
-public record HandleLobbyCreationCommand(GameId gameId, List<PlayerId> playerIds) {
-}
--- CheckersAchievementContext/src/main/java/be/kdg/integration5/checkersachievementcontext/port/in/HandleLobbyCreationUseCase.java
+++
@@ -1,5 +0,0 @@
-package be.kdg.integration5.checkersachievementcontext.port.in;
-
-public interface HandleLobbyCreationUseCase {
-    void handleLobbyCreation(HandleLobbyCreationCommand handleLobbyCreationCommand);
-}
