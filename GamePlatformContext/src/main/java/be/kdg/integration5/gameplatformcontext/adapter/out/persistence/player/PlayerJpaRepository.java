c335eb1854ac75ebf0c6847d39f5b285f676e95c
Sofiia Hmyria <sofiia.hmyria@student.kdg.be>
2025-01-09 16:25:56 +0100

    Testing for friends (send, accept, decline)

--- GamePlatformContext/src/main/java/be/kdg/integration5/gameplatformcontext/adapter/out/persistence/player/PlayerJpaRepository.java
+++ GamePlatformContext/src/main/java/be/kdg/integration5/gameplatformcontext/adapter/out/persistence/player/PlayerJpaRepository.java
@@ -1,13 +1,16 @@
 package be.kdg.integration5.gameplatformcontext.adapter.out.persistence.player;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface PlayerJpaRepository extends JpaRepository<PlayerJpaEntity, UUID> {
-
     List<PlayerJpaEntity> findAllByUsernameContainingIgnoreCase(String username);
+    PlayerJpaEntity findByPlayerId(UUID playerId);
 }
