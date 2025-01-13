3d632a984291439e6730ccba2a71a653dafdd231
Mykhailo Kruts <mykhailo.kruts@student.kdg.be>
2025-01-13 11:21:55 +0100

    Cors mappings from spring profiles

--- CheckersAchievementContext/src/main/java/be/kdg/integration5/checkersachievementcontext/adapter/config/WebConfig.java
+++ CheckersAchievementContext/src/main/java/be/kdg/integration5/checkersachievementcontext/adapter/config/WebConfig.java
@@ -1,5 +1,6 @@
 package be.kdg.integration5.checkersachievementcontext.adapter.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -9,10 +10,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
 
+    @Value("${platform.url}")
+    private String platformUrl;
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173")
+                .allowedOrigins(platformUrl)
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
--- StatisticsContext/src/main/java/be/kdg/integration5/statisticscontext/adapter/config/web/WebConfig.java
+++ StatisticsContext/src/main/java/be/kdg/integration5/statisticscontext/adapter/config/web/WebConfig.java
@@ -1,11 +1,30 @@
 package be.kdg.integration5.statisticscontext.adapter.config.web;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@EnableWebMvc
 @Configuration
-public class WebConfig {
+public class WebConfig implements WebMvcConfigurer {
+
+    @Value("${platform.url}")
+    private String platformUrl;
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins(platformUrl)
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+
+    }
+
     @Bean
     public WebClient.Builder webClientBuilder() {
         return WebClient.builder();
